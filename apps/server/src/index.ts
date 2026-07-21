45b861efd9174f18ea1f0a9b8cae8068873e3ee4
Rupesh Chaulagain <chaulagainrupesh1@gmail.com>
2026-07-21 11:13:56 +0545

    refactor code

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,24 +1,5 @@
-import { env } from "@durable-workflow-lab/env/server";
-import { Hono } from "hono";
-import { cors } from "hono/cors";
-import { logger } from "hono/logger";
-
-const app = new Hono();
-
-app.use(logger());
-app.use(
-  "/*",
-  cors({
-    origin: env.CORS_ORIGIN,
-    allowMethods: ["GET", "POST", "OPTIONS"],
-  }),
-);
-
-app.get("/", (c) => {
-  return c.text("OK");
-});
-
 import { serve } from "@hono/node-server";
+import app from "./app";
 
 serve(
   {
